630504e1196bce7a78e38643b21a68e9c00c5ec7
ciabby <97247200+ciabby@users.noreply.github.com>
2025-04-02 22:42:12 +0200

    modifiche
    fatte modifiche al codice

--- script.js
+++ script.js
@@ -18,15 +18,12 @@ function dadovevengo()
 function informazioni()
 {
     document.getElementById("info").style.border="5px solid black";
+    document.getElementById("info").style.borderRadius="20px 5px 20px 5px";
     document.getElementById("info").style.color="black";
     document.getElementById("info").textContent="il gioco consiste nel riuscire a abbattere tutte le barche avversarie riuscendo a trovarle tramite la posizione delle caselle";
-    const img = document.createElement("img");
-    img.src = "x.png";
-    img.style.position="absolute";
-    img.style.width="5%";
-    img.style.left="74%";
-    img.style.top="0%";
-    img.herf=""
-    img.style.position="absolute";
-    document.body.appendChild(img);
+    document.getElementById(x).style.display="flex";
+}
+function elimina()
+{
+    document.getElementById("info").remove();
 }
