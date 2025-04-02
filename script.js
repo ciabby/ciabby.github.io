308166952583c6f6151c2557af2c6065c635ebf6
ciabby <97247200+ciabby@users.noreply.github.com>
2025-04-02 19:39:16 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+function ideadigioco()
+{
+    alert("Volevamo creare un gioco abbastanza tranquillo, in grado di far divertire tutti i giocatori quindi che dire....");
+    alert("Buon divertimento");
+}
+function comecisonoarrivato()
+{
+    alert("è nato tutto per un progetto scolastico per poi essere uscito alla fine come: ' una idea troppo carina per essere lasciata cosi ' , quindi eccoci");
+}
+function chisono()
+{
+    alert("Emmanuele Loghitano ");
+}
+function dadovevengo()
+{
+    alert("Dall'Abramo Lincoln di Enna, settore informatica e telecomunicazioni")
+}
+function informazioni()
+{
+    document.getElementById("info").style.border="5px solid black";
+    document.getElementById("info").style.color="black";
+    document.getElementById("info").textContent="il gioco consiste nel riuscire a abbattere tutte le barche avversarie riuscendo a trovarle tramite la posizione delle caselle";
+    const img = document.createElement("img");
+    img.src = "x.png";
+    img.style.position="absolute";
+    img.style.width="5%";
+    img.style.left="74%";
+    img.style.top="0%";
+    img.herf=""
+    img.style.position="absolute";
+    document.body.appendChild(img);
+}
